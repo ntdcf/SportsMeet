94bb56cb5e1d22d366c95495f4e52220977835fb
luogkme <l_how@live.com>
2017-01-18 14:08:18 +0800

    添加登录activity

--- app/src/main/java/com/luohao/sportmeet/LoginActivity.java
+++ app/src/main/java/com/luohao/sportmeet/LoginActivity.java
@@ -3,15 +3,29 @@ package com.luohao.sportmeet;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class LoginActivity extends Activity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login);
+        button = (Button) findViewById(R.id.password_forget);
+        button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LoginActivity.this, "aaaaaaaaa", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
 //        getSupportActionBar().hide();;
     }
 }
